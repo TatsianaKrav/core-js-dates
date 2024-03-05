53a6ea38301a5a43b6b7d3a3e2dd975cac2a1647
unknown <tatsiana.krauchanka@gmail.com>
2024-03-05 12:40:36 +0300

    feat: complete getCountDaysOnPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -132,8 +132,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const date1 = new Date(dateStart);
+  const date2 = new Date(dateEnd);
+  return (date2 - date1) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
