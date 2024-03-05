b854473ebf0a52ca8d45f1b147c744df5eb5e30f
unknown <tatsiana.krauchanka@gmail.com>
2024-03-05 23:29:56 +0300

    feat: complete getQuarter task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -308,8 +308,21 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  let quarter = 0;
+
+  if (month >= 0 && month <= 2) {
+    quarter = 1;
+  } else if (month >= 3 && month <= 5) {
+    quarter = 2;
+  } else if (month >= 6 && month <= 8) {
+    quarter = 3;
+  } else if (month >= 9 && month <= 11) {
+    quarter = 4;
+  }
+
+  return quarter;
 }
 
 /**
